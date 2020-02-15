41860d7256f113c0d9bddea9acc2ffbc3067bb55
Marcin <MarcinObuszynski213@gmail.com>
2020-02-15 01:00:00 +0100

    Improving quality of tiles

--- src/main/java/GUI/Panel.java
+++ src/main/java/GUI/Panel.java
@@ -32,18 +32,37 @@ public class Panel extends JPanel {
         try {
             final JButton test = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tilesfinally.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tilesfixed2.png")));
             JButton test2 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tile1.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tile1fixed.png")));
+            final JButton test3 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tilesfinally.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tilesfixed2.png")));
+            JButton test4 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tile1.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tile1fixed.png")));
+            final JButton test5 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tilesfinally.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tilesfixed2.png")));
+            JButton test6 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tile1.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tile1fixed.png")));
+            final JButton test7 = new JButton(new ImageIcon(ResizeImages.resizeImage("C:\\Users\\Marcin\\Desktop\\tilesfinally.png", "C:\\Users\\Marcin\\Desktop\\Cmd\\tilesfixed2.png")));
 
-
+            test7.setBounds(296, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
+            test6.setBounds(250, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
+            test5.setBounds(204, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
+            test4.setBounds(158, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
+            test3.setBounds(112, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
             test2.setBounds(66, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
             test.setBounds(20, 10, SizeOfTiles.WIDTH.getValue(), SizeOfTiles.HEIGHT.getValue());
             test.setBorderPainted(false);
             test2.setBorderPainted(false);
+            test3.setBorderPainted(false);
+            test4.setBorderPainted(false);
+            test5.setBorderPainted(false);
+            test6.setBorderPainted(false);
+            test7.setBorderPainted(false);
             add(test);
             add(test2);
-        } catch (Exception e) {
+            add(test3);
+            add(test4);
+            add(test5);
+            add(test6);
+            add(test7);
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
-
     }
 
 
@@ -129,56 +148,70 @@ public class Panel extends JPanel {
         return false;
     }
 
-        public void paintComponent (Graphics g){
-            Graphics2D g2 = (Graphics2D) g;
+    public void paintComponent(Graphics g){
+        int y = 10;
+        for (int x = 20; x < 322; x += 46) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
             //left
             Polygon ml = new Polygon();
-            ml.addPoint(20, 10);
-            ml.addPoint(15, 20);
-            ml.addPoint(15, 80);
-            ml.addPoint(20, 70);
-            g2.setPaint(Color.GRAY);
-            g2.fillPolygon(ml);
+            ml.addPoint(x, y);
+            ml.addPoint(x - 5, y + 10);
+            ml.addPoint(x - 5, y + 70);
+            ml.addPoint(x, y + 60);
+            g2d.setPaint(Color.LIGHT_GRAY);
+            g2d.fillPolygon(ml);
+            g2d.setPaint(Color.GRAY);
+            g2d.drawLine(x, y + 60, x - 5, y + 69);
 
 
             //Middle-bottom
             Polygon mb = new Polygon();
-            mb.addPoint(20, 70);
-            mb.addPoint(15, 80);
-            mb.addPoint(61, 80);
-            mb.addPoint(66, 70);
-            g2.setPaint(Color.GRAY);
-
-            g2.fillPolygon(mb);
-
-
-            //top layer
-            Rectangle2D top = new Rectangle2D.Double(20, 10, 46, 60);
-
-            Polygon ml2 = new Polygon();
-            ml2.addPoint(66, 10);
-            ml2.addPoint(61, 20);
-            ml2.addPoint(61, 80);
-            ml2.addPoint(66, 70);
-            g2.setPaint(Color.GRAY);
-            g2.fillPolygon(ml2);
-
-
-            //Middle-bottom
-            Polygon mb2 = new Polygon();
-            mb2.addPoint(66, 70);
-            mb2.addPoint(61, 80);
-            mb2.addPoint(107, 80);
-            mb2.addPoint(113, 70);
-            g2.setPaint(Color.GRAY);
-
-            g2.fillPolygon(mb2);
+            mb.addPoint(x, y + 60);
+            mb.addPoint(x - 5, y + 70);
+            mb.addPoint(x + 41, y + 70);
+            mb.addPoint(x + 46, y + 60);
+            g2d.setPaint(Color.LIGHT_GRAY);
+            g2d.fillPolygon(mb);
 
 
             //top layer
-            Rectangle2D top2 = new Rectangle2D.Double(66, 10, 46, 60);
-            g2.fill(top);
+            Rectangle2D top = new Rectangle2D.Double(x, y, 46, 60);
         }
+//        Polygon ml2 = new Polygon();
+//        ml2.addPoint(66, 10);
+//        ml2.addPoint(61, 20);
+//        ml2.addPoint(61, 80);
+//        ml2.addPoint(66, 70);
+//        g2.setPaint(Color.LIGHT_GRAY);
+//        g2.fillPolygon(ml2);
+//
+//
+//        //Middle-bottom
+//        Polygon mb2 = new Polygon();
+//        mb2.addPoint(66, 70);
+//        mb2.addPoint(61, 80);
+//        mb2.addPoint(107, 80);
+//        mb2.addPoint(112, 70);
+//        g2.setPaint(Color.LIGHT_GRAY);
+//
+//        g2.fillPolygon(mb2);
+//
+//        g2.setColor(Color.MAGENTA);
+//        g2.drawLine(150, 140, 120, 80);
+//
+//
+//        //top layer
+//        Rectangle2D top2 = new Rectangle2D.Double(66, 10, 46, 60);
+//        g2.fill(top);
+    }
 
         public void setEnabled (ArrayList < Tile > allTilesinBoard) {
             for (Tile tile : allTilesinBoard)
